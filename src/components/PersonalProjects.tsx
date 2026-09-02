adf7eda53c5aa3b580b5281524ccb648df4e442a
kkk <seungyeung1@naver.com>
2026-09-02 21:21:24 +0900

    포트폴리오 이미지 경로수정

--- src/components/PersonalProjects.tsx
+++ src/components/PersonalProjects.tsx
@@ -59,7 +59,7 @@ function PersonalCard({ project, onSelect }: { project: Project; onSelect: (p: P
       {project.thumbnail && (
         <div className="-mx-6 -mt-6 mb-5 h-44 bg-[#06060f] overflow-hidden relative">
           <img
-            src={project.thumbnail}
+            src={`${import.meta.env.BASE_URL}${project.thumbnail.replace(/^\/+/, '')}`}
             alt={project.name}
             className="w-full h-full object-contain"
           />
